da78c414408817a6f3491cf8b48584eb7fa35d9f
lucyyingjuchu <lucyyingjuchu@gmail.com>
2025-09-14 16:40:15 -0700

    new print -ready updates

--- data/data_processing_scripts/Single_aws_uploader.py
+++ data/data_processing_scripts/Single_aws_uploader.py
@@ -8,7 +8,7 @@ processor = HybridFinerWorksProcessor()
 # Upload your first artwork
 base_path = r"C:\Users\paral\Documents\AdditionalFilesforMom-artSite\Original_image_collection"
 
-artwork_id = "019891b0-f39c-7b7d-8e54-83ae4f652136"
+artwork_id = "019891b0-f39c-7201-bdba-6943519e8862"
 file_ext=".jpg"
 
 local_file = os.path.join(base_path, artwork_id + file_ext)
---
+++ js/pinterest-tracking.js
@@ -0,0 +1,4 @@
+!function(e){if(!window.pintrk){window.pintrk = function () {
+window.pintrk.queue.push(Array.prototype.slice.call(arguments))};var n=window.pintrk;n.queue=[],n.version="3.0";var t=document.createElement("script");t.async=!0,t.src=e;var r=document.getElementsByTagName("script")[0]; r.parentNode.insertBefore(t,r)}}("https://s.pinimg.com/ct/core.js");
+pintrk('load', '2613487434745', {em: '<user_email_address>'});
+pintrk('page');
